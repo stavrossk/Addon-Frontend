3c2f3fb3cd8905777ada11f4922f4328185e9811
Franz Koch <franz.koch@elements-net.de>
2012-11-29 00:49:53 +0100

    [Task] add a CONTEXT constant that indicates the context the application is running in and load a different configuration file according to this context. To toggle the development context, simply create the file includes/developmentConfiguration.php containing your local configuration adoptions.

--- includes/configuration.php
+++ includes/configuration.php
@@ -2,13 +2,25 @@
 /**
  * This file contains the main configuration as simple array
  */
+
+// This sets the context the application is running in.
+// It allows to change certain behavior depending on context.
+// The currently supported contexts are:
+// development		Used in the development environment
+// production		Used on the live website
+$context = @is_file(dirname( __FILE__ ) . '/developmentConfiguration.php') ? 'development' : 'production';
+define('CONTEXT', $context);
+
+// The base configuration of the application
 $configuration = array(
+	// prepare dummy database settings (will be overridden by context depending configurations)
 	'database' => array(
 		'username'	=> '',
 		'password'	=> '',
 		'name'		=> '',
 		'server'	=> '',	
 	),
+	// defines some settings needed to interact with the repositories
 	'repository' => array(
 		'version' => 'eden',
 		'importUrl' => 'http://mirrors.xbmc.org/addons/eden/addons.xml',
@@ -16,5 +28,12 @@ $configuration = array(
 	)
 );
 
-require_once('/etc/xbmc/php-include/addons/private/configuration.php');
+
+// include the context depending configuration at the bottom
+// which allows to override any default configuration if needed
+if (CONTEXT == 'development') {
+	require_once('developmentConfiguration.php');
+} else {
+	require_once('/etc/xbmc/php-include/addons/private/configuration.php');
+}
 ?>
