ed6e310c908ea6bb5276ecb3cd7488740341c5f3
Franz Koch <franz.koch@elements-net.de>
2012-12-01 01:15:07 +0100

    [Bugfix] auto connect to DB on startup in order to have a valid connection available when calling $db->escape() which otherwise throws errors.

--- includes/db_connection.php
+++ includes/db_connection.php
@@ -3,6 +3,8 @@ require_once("ez_sql_core.php");
 require_once("ez_sql_mysql.php");
 
 if (isset($configuration['database'])) {
-	$db = new ezSQL_mysql($configuration['database']['username'], $configuration['database']['password'], $configuration['database']['name'], $configuration['database']['server']);
+	$db = new ezSQL_mysql();
+	// auto connect to the db in order to have a valid connection when calling $db->escape() for creating queries
+	$db->quick_connect($configuration['database']['username'], $configuration['database']['password'], $configuration['database']['name'], $configuration['database']['server']);
 }
 ?>
